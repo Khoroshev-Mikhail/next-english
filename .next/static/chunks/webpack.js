f31bbfef49dd2fdd6fe9d4242d63fc6f93b6754a
Mikha38 <oyks1379@yandex.ru>
2023-04-05 11:10:55 +0300

    home

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d55058337963954"; }
+/******/ 		__webpack_require__.h = function() { return "efdb3d9be9bc6801"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,12 +1,14 @@
+import { useSession } from "next-auth/react";
 import Header from "./Header";
 import Header_admin from "./Header_admin";
 
 export default function Layout({children}){
+    const { data: session } = useSession()
     return (
         <>
             <Header />
             <div className="container mx-auto min-h-screen  text-black text-lg ">
-                <Header_admin />
+                {/* {session?.user.role === 'ADMIN' && <Header_admin />} */}
                 <div className="pt-4">
                     {children}
                 </div>
