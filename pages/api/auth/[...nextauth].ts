58da68bd561dcc4711ddd75dda0574d2fc27dcd3
Mikha38 <oyks1379@yandex.ru>
2023-04-07 23:25:18 +0300

    home

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import prisma from '../../../lib/prismadb';
 import type { NextAuthOptions } from 'next-auth'
 import GoogleProvider from "next-auth/providers/google";
+import VkProvider from "next-auth/providers/vk";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions: NextAuthOptions = {
@@ -12,6 +13,10 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
+        VkProvider({
+            clientId: process.env.VK_CLIENT_ID,
+            clientSecret: process.env.VK_CLIENT_SECRET
+          })
     ],
     callbacks: {
         session({ session, token, user }) {
